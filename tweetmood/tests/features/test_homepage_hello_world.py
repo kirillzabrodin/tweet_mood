0d719f5f73b9d0797dc14f7d1a70b65b71862d79
Chris Gilbert <chrisgilbert4@icloud.com>
2019-01-22 15:37:32 +0000

    hello world smoke test passing

---
+++ tweetmood/tests/features/test_homepage_hello_world.py
@@ -0,0 +1,22 @@
+from django.test import LiveServerTestCase
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+class AccountTestCase(LiveServerTestCase):
+
+    def setUp(self):
+        self.selenium = webdriver.Firefox()
+        super(AccountTestCase, self).setUp()
+
+    def tearDown(self):
+        self.selenium.quit()
+        super(AccountTestCase, self).tearDown()
+
+    def test_register(self):
+        selenium = self.selenium
+        #Opening the link we want to test
+        selenium.get(self.live_server_url)
+        #find the form element
+        body_text = selenium.find_element_by_tag_name('body').text
+        #check the returned result
+        assert 'Hello world' in body_text
