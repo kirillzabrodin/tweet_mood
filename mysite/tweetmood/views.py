c2bfeebec306ea469b5f4a6c913e82b593caac83
topickering <topickering@gmail.com>
2019-01-22 11:30:04 +0000

    Views and routes: linked submit button to text page, which pulls and displays text parameter

--- mysite/tweetmood/views.py
+++ mysite/tweetmood/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 
 
 def index(request):
     return render(request, 'tweetmood/index.html')
+
+
+def text(request):
+    text = request.POST['text']
+    return HttpResponse("You submitted " + text)
