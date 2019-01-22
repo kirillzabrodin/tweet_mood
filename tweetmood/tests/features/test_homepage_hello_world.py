d2bb2614d3b7ee1032d28a742a7a2313914e9e34
Chris Gilbert <chrisgilbert4@icloud.com>
2019-01-22 17:52:34 +0000

    removed comments

--- tweetmood/tests/features/test_homepage_hello_world.py
+++ tweetmood/tests/features/test_homepage_hello_world.py
@@ -16,9 +16,6 @@ class HelloWorldTest(LiveServerTestCase):
 
     def test_register(self):
         selenium = self.selenium
-        #Opening the link we want to test
         selenium.get(self.live_server_url)
-        #find the form element
         body_text = selenium.find_element_by_tag_name('body').text
-        #check the returned result
         assert 'Hello world' in body_text
