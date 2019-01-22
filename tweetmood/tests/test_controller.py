88a6b334907a3fca33686d7f6c89cebe7ef326a6
Kirill Zabrodin <kirillvzabrodin@gmail.com>
2019-01-22 11:55:44 +0000

    wdb migrated

--- tweetmood/tests/test_controller.py
+++ tweetmood/tests/test_controller.py
@@ -6,5 +6,4 @@ class Webpage(TestCase):
     def test_root_irl_resolves_to_main_page(self):
         client = Client()
         response = client.get('/')
-        # self.assertEqual(found.func, index)
         self.assertEqual(response.status_code, 200)
