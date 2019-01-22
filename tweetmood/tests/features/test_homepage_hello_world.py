cee51c59cc79d2dc0e439eb53c03f2abce213c0f
Chris Gilbert <chrisgilbert4@icloud.com>
2019-01-22 17:48:25 +0000

    removed keys

--- tweetmood/tests/features/test_homepage_hello_world.py
+++ tweetmood/tests/features/test_homepage_hello_world.py
@@ -1,6 +1,5 @@
 from django.test import LiveServerTestCase
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.firefox.options import Options
 
 class HelloWorldTest(LiveServerTestCase):
