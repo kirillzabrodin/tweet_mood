c592d550358463fdb8cc9b7a2b01f0d1102a30e5
Chris Gilbert <chrisgilbert4@icloud.com>
2019-01-23 20:59:00 +0000

    changed ToneAnalyzer class name to Watson and written first Watson unit tests

--- tweetmood/tests/test_watson.py
+++ tweetmood/tests/test_watson.py
@@ -1,27 +1,14 @@
 from django.test import TestCase
 import unittest
-from doubles import ObjectDouble, allow, expect
 from unittest import mock
 from unittest.mock import patch
 from watson_developer_cloud import ToneAnalyzerV3
 from ..watson import Watson
-from unittest.mock import MagicMock
 
-class WatsonUnitTests(unittest.TestCase):
+class WatsonUnitTests(TestCase):
     def test_send_for_analysis(self):
-        tone_analyzer = MagicMock()
-        watson = Watson(tone_analyzer)
-        watson.send_for_analysis('text')
-        tone_analyzer.tone.assert_called_with({ 'text': 'text' },
-        'application/json')
-
-#  1
-# expect subject to respond to send_for_analysis
-
-#  2
-# allow tone_analyzer double to receive get_result()
-# expect the tone_analyzer double to receive tone()
-
-# 3
-# allow tone_analyzer double to receive tone().and_return('text analysis')
-# expect the tone_analyzer to receive get_result()
+        with mock.patch('watson_developer_cloud.ToneAnalyzerV3') as mock_analyzer:
+            watson = Watson(mock_analyzer)
+            watson.send_for_analysis('test text')
+            mock_analyzer.tone.assert_called_once_with({ 'text': 'test text' },
+            'application/json')
--- tweetmood/views.py
+++ tweetmood/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from .watson import Watson
+import json
 
 def index(request):
     return render(request, 'tweetmood/index.html')
@@ -8,5 +9,5 @@ def index(request):
 def analysis(request):
     text = request.GET['text']
     watson = Watson()
-    analysed_text = watson.send_for_analysis(text)
+    analysed_text = json.dumps(watson.send_for_analysis(text))
     return HttpResponse("You submitted:<br>" + text + "<br>" + "Your results:<br>" + analysed_text)
