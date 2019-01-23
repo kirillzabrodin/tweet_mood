0b23baa20b415f94564d2b4b2fc998e47bcc376c
Kirill Zabrodin <kirillvzabrodin@gmail.com>
2019-01-23 11:28:25 +0000

    changed static root definition

--- mysite/settings.py
+++ mysite/settings.py
@@ -18,7 +18,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, 'staticfiles'))
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
