60ce1b9aa0c26f2c12c318d236fcb8d791cbb79e
Chris Gilbert <chrisgilbert4@icloud.com>
2019-01-23 21:01:02 +0000

    tidied up watson unit test

--- tweetmood/tests/test_watson.py
+++ tweetmood/tests/test_watson.py
@@ -5,10 +5,10 @@ from unittest.mock import patch
 from watson_developer_cloud import ToneAnalyzerV3
 from ..watson import Watson
 
+@patch('watson_developer_cloud.ToneAnalyzerV3')
 class WatsonUnitTests(TestCase):
-    def test_send_for_analysis(self):
-        with mock.patch('watson_developer_cloud.ToneAnalyzerV3') as mock_analyzer:
-            watson = Watson(mock_analyzer)
-            watson.send_for_analysis('test text')
-            mock_analyzer.tone.assert_called_once_with({ 'text': 'test text' },
-            'application/json')
+    def test_send_for_analysis(self, mock_analyzer):
+        watson = Watson(mock_analyzer)
+        watson.send_for_analysis('test text')
+        mock_analyzer.tone.assert_called_once_with({ 'text': 'test text' },
+        'application/json')
