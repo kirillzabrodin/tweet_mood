2de035c4d29b220c97e71f99c19d6fa9e964ca79
Chris Gilbert <chrisgilbert4@icloud.com>
2019-01-24 10:10:01 +0000

    improved watson unit test to cover get_result call as well

--- tweetmood/tests/test_watson.py
+++ tweetmood/tests/test_watson.py
@@ -1,14 +1,18 @@
 from django.test import TestCase
 import unittest
 from unittest import mock
-from unittest.mock import patch
+from unittest.mock import patch, Mock
 from watson_developer_cloud import ToneAnalyzerV3
 from ..watson import Watson
 
-@patch('watson_developer_cloud.ToneAnalyzerV3')
 class WatsonUnitTests(TestCase):
-    def test_send_for_analysis(self, mock_analyzer):
-        watson = Watson(mock_analyzer)
+    def test_send_for_analysis(self):
+        mock_response_object = Mock()
+        mock_response_object.get_result = Mock(return_value={})
+        mock_tone_analyzer = Mock()
+        mock_tone_analyzer.tone = Mock(return_value=mock_response_object)
+        watson = Watson(mock_tone_analyzer)
         watson.send_for_analysis('test text')
-        mock_analyzer.tone.assert_called_once_with({ 'text': 'test text' },
+        mock_tone_analyzer.tone.assert_called_once_with({ 'text': 'test text' },
         'application/json')
+        mock_response_object.get_result.assert_called_once()
