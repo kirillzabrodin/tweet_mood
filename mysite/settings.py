9987c9d52bdcc742f501315dd78d5f097153a936
Kirill Zabrodin <kirillvzabrodin@gmail.com>
2019-01-24 16:39:54 +0000

    added debug mode back

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('TWEETMOOD_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['0.0.0.0', 'localhost', 'https://tweet-mood.herokuapp.com/']
 
