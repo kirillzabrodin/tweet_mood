753fafd9ec0e305ed3c9696a264c9a660c3de527
Kirill Zabrodin <kirillvzabrodin@gmail.com>
2019-01-24 16:29:52 +0000

    finished heroku and travis setup

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -113,13 +114,10 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.9/howto/static-files/
-print("Hello heroku")
+# https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = "/staticfoo/"
-print("static root: |" + STATIC_ROOT + "|")
-print(STATIC_ROOT)
+
 # Configure Django App for Heroku.
-import django_heroku
 django_heroku.settings(locals())
