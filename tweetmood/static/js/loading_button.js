f83528adbccf7bf6e273294de5b5887c50efa237
James de Winton <james.dewinton@gmail.com>
2019-01-25 16:13:18 +0000

    added loading function when button pressed

---
+++ tweetmood/static/js/loading_button.js
@@ -0,0 +1,6 @@
+window.addEventListener('load', function(){
+  document.getElementById("button_loader").addEventListener("click", function(){
+    document.getElementById("button_loader").innerHTML = "Loading...";
+  });
+
+});
