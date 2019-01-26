dd9f7203b78616d9003cb67c686b48c65e431bd6
topickering <topickering@gmail.com>
2019-01-26 14:32:49 +0000

    Holmes: unit tests for all outcomes.  Coverage 100%

--- tweetmood/holmes/holmes.py
+++ tweetmood/holmes/holmes.py
@@ -3,8 +3,11 @@ from textblob import TextBlob
 
 class Holmes:
 
+    def __init__(self, textblob = TextBlob()):
+        self.textblob = textblob
+
     def holmes_analysis(self, text):
-        analysis = TextBlob(text)
+        analysis = self.textblob(text)
         score = analysis.sentiment.polarity
         if score >= 0.5:
             return "very positive"
