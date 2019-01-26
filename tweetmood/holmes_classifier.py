f46adda6b1555bb316dd4c4d9aacf9873d850804
topickering <topickering@gmail.com>
2019-01-26 19:12:03 +0000

    Skeleton code for different classifier basis

--- tweetmood/holmes_classifier.py
+++ tweetmood/holmes_classifier.py
@@ -1,5 +1,5 @@
-# from textblob import TextBlob
-# from textblob.classifiers import NaiveBayesClassifier
+from textblob import TextBlob
+from textblob.classifiers import NaiveBayesClassifier
 # from nltk.corpus import movie_reviews
 # import random
 
@@ -24,3 +24,35 @@
 
 # This runs text through the classifier and returns pos or neg:
 # print(classifier.classify("I love the world"))
+train = [
+     ('I love this sandwich.', 'pos'),
+     ('this is an amazing place!', 'pos'),
+     ('I feel very good about these beers.', 'pos'),
+     ('this is my best work.', 'pos'),
+     ("what an awesome view", 'pos'),
+     ('I do not like this restaurant', 'neg'),
+     ('I am tired of this stuff.', 'neg'),
+     ("I can't deal with this", 'neg'),
+     ('he is my sworn enemy!', 'neg'),
+     ('my boss is horrible.', 'neg'),
+     ('I am neutral', 'neu'),
+     ('this is average', 'neu'),
+     ('i am here', 'neu')
+ ]
+test = [
+     ('the beer was good.', 'pos'),
+     ('I do not enjoy my job', 'neg'),
+     ("I ain't feeling dandy today.", 'neg'),
+     ("I feel amazing!", 'pos'),
+     ('Gary is a friend of mine.', 'pos'),
+     ("I can't believe I'm doing this.", 'neg'),
+     ('today is monday', 'neu')
+ ]
+
+cl = NaiveBayesClassifier(train)
+print(cl.classify("I love the world"))
+prob_dist = (cl.prob_classify("I love the world"))
+print(prob_dist.max())
+print(round(prob_dist.prob("pos"), 2))
+print(round(prob_dist.prob("neg"), 2))
+print(round(prob_dist.prob("neu"), 2))
