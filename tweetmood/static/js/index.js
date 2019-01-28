16b2a4328b017274be9eb19e9e73b2d8ad533a48
Chris Gilbert <chrisgilbert4@icloud.com>
2019-01-28 15:32:41 +0000

    displaying progress bars now using ajax

--- tweetmood/static/js/index.js
+++ tweetmood/static/js/index.js
@@ -19,6 +19,7 @@ $(document).ready(function() {
   function displayResults(data) {
     var url = "http://localhost:8000/result"
     $.get(url, function(data) {
+      $('#results').show()
       displayUsersInputText(data.text)
       displayProgressBars(data.response_dict)
     })
@@ -33,13 +34,11 @@ $(document).ready(function() {
   }
 
   function displayProgressBars(data) {
-    $("#joy").attr('style', 'width: ' + data['Joy'] + '%' ).attr('aria-valuenow', data['Joy'])
-    $("#anger")
-    $("#sadness")
-    $("#analytical")
-    $("#fear")
-    $("#disgust")
-    $("#confident")
-    $("#tentative")
+    for(var tone in data) {
+      var toneName = tone
+      var toneScore = data[tone]
+      $('#progress-results').append(`<h1>${toneName} - ${toneScore}%</h1><div id='progress-results' class='progress'><div id='${toneName}' class='progress-bar progress-bar-${toneName}' role='progressbar' style='width: ${toneScore}%' aria-valuenow='${toneScore}' aria-valuemin='0' aria-valuemax='100'></div>'</div>`)
+    }
   }
+
 })
