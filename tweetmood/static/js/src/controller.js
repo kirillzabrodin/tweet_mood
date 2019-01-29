9d0104055600f961ac194dcc7cf74fd152f1455f
Chris Gilbert <chrisgilbert4@icloud.com>
2019-01-29 18:09:23 +0000

    spa implemented

--- tweetmood/static/js/src/controller.js
+++ tweetmood/static/js/src/controller.js
@@ -1,34 +1,69 @@
 (function(exports) {
 
-  function Controller(homepageView = new HomepageView) {
+  function Controller(resultsView = new ResultsView, homepageView = new HomepageView) {
+    this.resultsView = resultsView
     this.homepageView = homepageView
   }
 
   Controller.prototype = {
     displayHomepage: function() {
-      this._hideresults()
-      $('#london-input-form').html(this.homepageView.render())
-      this._listenForLondonFormSubmit()
+      var self = this
+      self._hideresults()
+      $('#london-input-form').html(self.homepageView.render())
+      self._listenForLondonFormSubmit()
     },
 
     _listenForLondonFormSubmit: function() {
       var self = this
       $('#london-form').submit(function(e) {
         e.preventDefault()
-        console.log('fuck off jasmine')
-        self.displayLoadingButton()
-        // self.postFormData($('#london-form'))
+        self._displayLoadingButton()
+        self._postFormData($('#london-form'))
       })
     },
 
-    displayLoadingButton: function() {
+    _postFormData: function(form) {
+      $.ajax({
+        type: form.attr('method'),
+        url: form.attr('action'),
+        data: form.serialize(),
+        success: (data) => {
+          this._hideInputForm()
+          this._getResults(data)
+        }
+      })
+    },
 
+    _getResults: function(data) {
+      var url = "http://localhost:8000/result"
+      $.get(url, (data) => {
+        this._displayResults(data)
+      })
+    },
+
+    _displayResults: function(data) {
+      var self = this
+      var text = data.text
+      var response_dict = data.response_dict
+      $('#results').show()
+      $('#results').append(self.resultsView.renderUsersInputText(text))
+      $('#results').append(self.resultsView.renderProgressDiv())
+      $('#progress-results').append(self.resultsView.renderProgressBars(response_dict))
+    },
+
+    _displayLoadingButton: function() {
+      var self = this
+      $('#button-spinner').addClass(self.homepageView.form.button.renderSpinnerClass())
+      $('#button-text').text(self.homepageView.form.button.renderLoadingText())
+    },
+
+    _hideInputForm: function() {
+      $("#london-input-form").hide()
     },
 
     _hideresults: function() {
       $('#results').hide()
     }
-
 
   }
 
