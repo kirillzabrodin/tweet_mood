e8aab8bd46d2fb2a2f09fd8889abc1c8c2f4741a
Kirill Zabrodin <kirillvzabrodin@gmail.com>
2019-01-29 14:53:14 +0000

    all tests passing with mocking

--- tweetmood/tests/test_tweeterpy.py
+++ tweetmood/tests/test_tweeterpy.py
@@ -1,20 +1,48 @@
 from django.test import TestCase
 import unittest
 from unittest import mock
-from unittest.mock import patch, Mock, ANY
+from unittest.mock import patch, Mock, ANY, MagicMock
 from ..tweeterpy import Tweeterpy
+import tweepy
 
 class TweeterpyUnitTests(TestCase):
-    def Strips_hashtags_links_and_ats(self):
-        tweets = Tweeterpy()
-        result = tweets.format_output("RT #hello @testing https://www.test-this.com")
-        assert result == "   "
 
-    # def test_send_for_analysis_calls_get_result(self):
-    #     mock_response_object = Mock()
-    #     mock_response_object.get_result = Mock(return_value={})
-    #     mock_tone_analyzer = Mock()
-    #     mock_tone_analyzer.analyze = Mock(return_value=mock_response_object)
-    #     watson = Watson(mock_tone_analyzer)
-    #     watson.send_for_analysis('test text', 'test')
-    #     mock_response_object.get_result.assert_called_once()
+    def iterable(self, text):
+        class RandItem:
+            def __init__(self, text):
+                self.text = text
+        item = RandItem(text)
+        list = [item]
+        return list
+
+    def test_strips_hashtags(self):
+        tweets = Tweeterpy()
+        result_hashtag = tweets.strip_all_entities("#hello")
+        assert result_hashtag == ""
+
+    def test_strips_ats(self):
+        tweets = Tweeterpy()
+        result_at = tweets.strip_all_entities("@testing")
+        assert result_at == ""
+
+    def test_strips_urls(self):
+        tweets = Tweeterpy()
+        result_url = tweets.strip_links("https://www.test-this.com")
+        assert result_url == ""
+
+    def test_strips_rt(self):
+        tweets = Tweeterpy()
+        result_rt = tweets.strip_rt("RT")
+        assert result_rt == ""
+
+    def test_does_not_delete_words(self):
+        tweets = Tweeterpy()
+        result_word = tweets.format_output("RT #hi hello there Jeff! https://www.test-this.com @itworks")
+        assert result_word == "hello there Jeff!"
+
+    def test_send_for_tweets_and_get_result(self):
+        mock_response_object = Mock()
+        mock_response_object.items = Mock(return_value=self.iterable('Tweet'))
+        tweepy.Cursor = Mock(return_value=mock_response_object)
+        tweets = Tweeterpy()
+        assert tweets.get_tweets('test') == "Tweet"
--- tweetmood/tweeterpy.py
+++ tweetmood/tweeterpy.py
@@ -7,41 +7,48 @@ import string
 
 class Tweeterpy:
 
-    def __init__(self):
-        auth = tweepy.OAuthHandler(os.environ.get('CONSUMER_TWITTER_KEY'), os.environ.get('CONSUMER_SECRET_TWITTER_KEY'))
-        auth.set_access_token(os.environ.get('TWITTER_ACCESS_TOKEN'), os.environ.get('TWITTER_ACCESS_TOKEN_SECRET'))
-        self.api = tweepy.API(auth)
+    ACCESS_TOKEN = os.environ.get('TWITTER_ACCESS_TOKEN')
+    ACCESS_TOKEN_SECRET = os.environ.get('TWITTER_ACCESS_TOKEN_SECRET')
+    CONSUMER_KEY = os.environ.get('CONSUMER_TWITTER_KEY')
+    CONSUMER_SECRET_KEY = os.environ.get('CONSUMER_SECRET_TWITTER_KEY')
 
-    def strip_links(text):
+    def __init__(self, tweets = tweepy,
+        auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET_KEY),
+        ACCESS_TOKEN = ACCESS_TOKEN,
+        ACCESS_TOKEN_SECRET = ACCESS_TOKEN_SECRET
+    ):
+        auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+        self.api = tweepy.API(auth)
+        self.tweets = tweepy
+
+    def strip_links(self, text):
         link_regex    = re.compile('((https?):((//)|(\\\\))+([\w\d:#@%/;$()~_?\+-=\\\.&](#!)?)*)', re.DOTALL)
         links         = re.findall(link_regex, text)
         for link in links:
-            text = text.replace(link[0], ', ')
+            text = text.replace(link[0], '')
         return text
 
-    def strip_all_entities(text):
+    def strip_all_entities(self, text):
         entity_prefixes = ['@','#']
-        for separator in  string.punctuation:
-            if separator not in entity_prefixes :
-                text = text.replace(separator,' ')
         words = []
         for word in text.split():
-            word = word.strip()
+            word = word.strip() #removes whitespace
             if word:
                 if word[0] not in entity_prefixes:
                     words.append(word)
         return ' '.join(words)
 
-    def strip_RT(text):
+    def strip_rt(self, text):
         return text.replace('RT', '')
 
-    def format_output(text):
-        return Tweeterpy.strip_all_entities(Tweeterpy.strip_links(Tweeterpy.strip_RT(text)))
+    def format_output(self, text):
+        return self.strip_all_entities(self.strip_links(self.strip_rt(text)))
 
     def get_tweets(self, text):
-        result = tweepy.Cursor(self.api.search, q=text, geocode="51.5074,0.1278,20km", lang='en',rpp='100',result_type='recent').items(100)
+        result = self.tweets.Cursor(self.api.search, q=text, geocode="51.5074,0.1278,20km", lang='en',rpp='100',result_type='recent').items(100)
         raw_output = ''
         for tweet in result:
+            print(tweet)
             raw_output += " " + tweet.text
-        output = Tweeterpy.format_output(raw_output)
+        output = self.format_output(raw_output)
         return output
