1644b2c8a09185aaf69cdd2f82f96f24c096144b
James de Winton <james.dewinton@gmail.com>
2019-01-29 16:20:14 +0000

    cypress errors

--- tweetmood/static/js/loading_button.js
+++ tweetmood/static/js/loading_button.js
@@ -1,4 +1,5 @@
 window.addEventListener('load', function(){
+  console.log(document.getElementById("button_loader"))
   document.getElementById("button_loader").addEventListener("click", function(){
     document.getElementById("button_loader").innerHTML = "Loading...";
   });
