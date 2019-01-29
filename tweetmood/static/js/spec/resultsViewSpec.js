45b9110862656441bb69b8d3bb1e9a6791a26c8c
Chris Gilbert <chrisgilbert4@icloud.com>
2019-01-29 10:24:13 +0000

    results page render progress div on green

--- tweetmood/static/js/spec/resultsViewSpec.js
+++ tweetmood/static/js/spec/resultsViewSpec.js
@@ -7,10 +7,18 @@ describe("ResultsView", function() {
   })
 
   describe("renderProgressBars", function() {
-    it("renders out progress bars", function() {
+    it("returns html of progress bars with data added", function() {
       var data = { "Joy" : 10, "Anger" : 0  }
       result = resultsView.renderProgressBars(data)
       expectedResult = "<h1>Joy - 10%</h1><div id='progress-results' class='progress'><div id='Joy' class='progress-bar progress-bar-Joy' role='progressbar' style='width: 10%' aria-valuenow='10' aria-valuemin='0' aria-valuemax='100'></div>'</div><h1>Anger - 0%</h1><div id='progress-results' class='progress'><div id='Anger' class='progress-bar progress-bar-Anger' role='progressbar' style='width: 0%' aria-valuenow='0' aria-valuemin='0' aria-valuemax='100'></div>'</div>"
+      expect(result).toEqual(expectedResult)
+    })
+  })
+
+  describe("renderProgressResultsDiv", function() {
+    it("returns progress results div html", function() {
+      var result = resultsView.renderProgressDiv()
+      var expectedResult = '<div id="progress-results"></div>'
       expect(result).toEqual(expectedResult)
     })
   })
--- tweetmood/static/js/src/resultsView.js
+++ tweetmood/static/js/src/resultsView.js
@@ -11,23 +11,17 @@
         results.push(`<h1>${toneName} - ${toneScore}%</h1><div id='progress-results' class='progress'><div id='${toneName}' class='progress-bar progress-bar-${toneName}' role='progressbar' style='width: ${toneScore}%' aria-valuenow='${toneScore}' aria-valuemin='0' aria-valuemax='100'></div>'</div>`)
       }
       return results.join("")
+    },
+
+    renderProgressDiv: function() {
+      return '<div id="progress-results"></div>'
     }
   }
 
   exports.ResultsView = ResultsView
 })(this)
 
-// export class ResultsView {
-//
-//   renderProgressBars(data) {
-//     let results = []
-//     for(var tone in data) {
-//       let toneName = tone
-//       let toneScore = data[tone]
-//       results.push(`<h1>${toneName} - ${toneScore}%</h1><div id='progress-results' class='progress'><div id='${toneName}' class='progress-bar progress-bar-${toneName}' role='progressbar' style='width: ${toneScore}%' aria-valuenow='${toneScore}' aria-valuemin='0' aria-valuemax='100'></div>'</div>`)
-//     }
-//     return results.join("")
-//   }
+
 //
 //   renderProgressResultsDiv() {
 //     return '<div id="progress-results"></div>'
