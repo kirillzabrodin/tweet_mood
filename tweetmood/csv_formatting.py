c5b8f575105d59a4bb1721ab0521ae53936f8c9d
topickering <topickering@gmail.com>
2019-01-29 10:32:35 +0000

    Editing some tweets in library

--- tweetmood/csv_formatting.py
+++ tweetmood/csv_formatting.py
@@ -1,8 +1,8 @@
-# import csv
-#
-# with open("tweetmood/trainingsample.csv","rt") as source:
-#     rdr= csv.reader( source )
-#     with open("tweetmood/formatted_train.csv","wt") as result:
-#         wtr= csv.writer( result )
-#         in_iter= ( (r[5], r[0]) for r in rdr )
-#         wtr.writerows( in_iter )
+import csv
+
+with open("tweetmood/trainingsample.csv", "rt") as source:
+    rdr = csv.reader(source)
+    with open("tweetmood/formatted_train.csv", "wt") as result:
+        wtr = csv.writer(result)
+        in_iter = ((r[5], r[0]) for r in rdr)
+        wtr.writerows(in_iter)
