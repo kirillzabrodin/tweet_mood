db25460ac8e811a1b1aed85986dd4a459034e41a
Chris Gilbert <chrisgilbert4@icloud.com>
2019-01-31 21:17:28 +0000

    switched results view if statement round to make more reaable

--- tweetmood/static/js/src/resultsView.js
+++ tweetmood/static/js/src/resultsView.js
@@ -5,16 +5,15 @@
   ResultsView.prototype = {
 
     render: function(user_text, watson_res, holmes_res) {
-      if (holmes_res.feeling) {
+      if (!holmes_res.feeling) {
+        return ErrorMessage.render(user_text)
+      } else {
         return [
           HolmesHeader.render(user_text, holmes_res),
           HolmesResults.render(holmes_res),
           WatsonHeader.render(),
           WatsonResults.render(watson_res)
         ].join("")
-      }
-      else {
-        return ErrorMessage.render(user_text)
       }
     }
 
