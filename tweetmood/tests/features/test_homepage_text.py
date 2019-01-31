149521972c0a894b9f59a84f7407822cac500064
Chris Gilbert <chrisgilbert4@icloud.com>
2019-01-31 14:47:43 +0000

    uopdated homepage feature tests to reflect updated spa views

--- tweetmood/tests/features/test_homepage_text.py
+++ tweetmood/tests/features/test_homepage_text.py
@@ -1,22 +1,39 @@
 from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
+from unittest.mock import patch
 
 
-class HelloWorldTest(LiveServerTestCase):
+class HomepageTest(LiveServerTestCase):
 
     def setUp(self):
         options = Options()
         options.add_argument('-headless')
         self.selenium = webdriver.Firefox(options=options)
-        super(HelloWorldTest, self).setUp()
+        super(HomepageTest, self).setUp()
 
     def tearDown(self):
         self.selenium.quit()
-        super(HelloWorldTest, self).tearDown()
+        super(HomepageTest, self).tearDown()
 
     def test_how_does_london_feel(self):
         selenium = self.selenium
         selenium.get(self.live_server_url)
-        body_text = selenium.find_element_by_tag_name('body').text
-        assert 'How does London feel about right now?' in body_text
+        form_text = selenium.find_element_by_tag_name('form').text
+        assert 'How does London feel about right now?' in form_text
+
+    def test_button_text(self):
+        selenium = self.selenium
+        selenium.get(self.live_server_url)
+        button_text = selenium.find_element_by_tag_name('button').text
+        assert 'click to find out' in button_text
+
+    @patch('tweetmood.views.analysis')
+    def test_loading_button_text(self, mock_view_response):
+        selenium = self.selenium
+        selenium.get(self.live_server_url)
+        text_field = selenium.find_element_by_name('text')
+        text_field.send_keys('Test')
+        selenium.find_element_by_name('analyse').click()
+        button_text = selenium.find_element_by_tag_name('button').text
+        assert 'loading...' in button_text
