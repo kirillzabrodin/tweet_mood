e6b25ca66a977ecee21940c0d548a84be2ac6b47
Chris Gilbert <chrisgilbert4@icloud.com>
2019-01-31 21:14:08 +0000

    results view tests improved to include if statement conditions

--- tweetmood/static/js/spec/resultsViewSpec.js
+++ tweetmood/static/js/spec/resultsViewSpec.js
@@ -1,48 +1,89 @@
 describe("ResultsView", function() {
 
   var resultsView
+  var userText
+  var watsonRes
+  var holmesRes
 
-  beforeEach(function() {
-    resultsView = new ResultsView()
-  })
+  describe("render", function() {
 
-  describe("_renderHolmesHeader", function() {
-    it("calls HolmesHeader.render", function() {
-      spyOn(HolmesHeader, "render")
-      resultsView._renderHolmesHeader("text", "holmes results")
-      expect(HolmesHeader.render).toHaveBeenCalledWith("text", "holmes results")
+    describe("when holmes gives response with feeling", function() {
+
+      beforeEach(function() {
+        userText = "foo"
+        watsonRes = {"joy": 10, "anger": 0}
+        holmesRes = {"pos": 50, "neg": 50, "pwid": 25, "nwid": 25, "feeling": "ambivalent"}
+        resultsView = new ResultsView()
+      })
+
+      it("calls HolmesHeader.render", function() {
+        spyOn(HolmesHeader, "render")
+        resultsView.render(userText, watsonRes, holmesRes)
+        expect(HolmesHeader.render).toHaveBeenCalledWith(userText, holmesRes)
+      })
+
+      it("calls HolmesResults.render", function() {
+        spyOn(HolmesResults, "render")
+        resultsView.render(userText, watsonRes, holmesRes)
+        expect(HolmesResults.render).toHaveBeenCalledWith(holmesRes)
+      })
+
+      it("calls WatsonHeader.render", function() {
+        spyOn(WatsonHeader, "render")
+        resultsView.render(userText, watsonRes, holmesRes)
+        expect(WatsonHeader.render).toHaveBeenCalledWith()
+      })
+
+      it("calls WatsonResults.render", function() {
+        spyOn(WatsonResults, "render")
+        resultsView.render(userText, watsonRes, holmesRes)
+        expect(WatsonResults.render).toHaveBeenCalledWith(watsonRes)
+      })
+
+      it("does not call ErrorMessage.render", function() {
+        spyOn(ErrorMessage, "render")
+        resultsView.render(userText, watsonRes, holmesRes)
+        expect(ErrorMessage.render).not.toHaveBeenCalledWith(userText)
+      })
     })
-  })
 
-  describe("_renderHolmesResult", function() {
-    it("calls HolmesResults.render", function() {
-      spyOn(HolmesResults, "render")
-      resultsView._renderHolmesResult("holmes results")
-      expect(HolmesResults.render).toHaveBeenCalledWith("holmes results")
-    })
-  })
+    describe("when holmes has no feeling", function() {
+      beforeEach(function() {
+        userText = "foo"
+        watsonRes = {"joy": 10, "anger": 0}
+        holmesRes = {}
+        resultsView = new ResultsView()
+      })
 
-  describe("_renderWatsonHeader", function() {
-    it("calls WatsonHeader.render", function() {
-      spyOn(WatsonHeader, "render")
-      resultsView._renderWatsonHeader("data")
-      expect(WatsonHeader.render).toHaveBeenCalledWith("data")
-    })
-  })
+      it("does not call HolmesHeader.render", function() {
+        spyOn(HolmesHeader, "render")
+        resultsView.render(userText, watsonRes, holmesRes)
+        expect(HolmesHeader.render).not.toHaveBeenCalledWith(userText, holmesRes)
+      })
 
-  describe("_renderWatsonResults", function() {
-    it("calls WatsonResults.render", function() {
-      spyOn(WatsonResults, "render")
-      resultsView._renderWatsonResult("watson results")
-      expect(WatsonResults.render).toHaveBeenCalledWith("watson results")
-    })
-  })
+      it("does not call HolmesResults.render", function() {
+        spyOn(HolmesResults, "render")
+        resultsView.render(userText, watsonRes, holmesRes)
+        expect(HolmesResults.render).not.toHaveBeenCalledWith(holmesRes)
+      })
 
-  describe("_renderErrorMessage", function() {
-    it("calls ErrorMessage.render", function() {
-      spyOn(ErrorMessage, "render")
-      resultsView._renderErrorMessage("error")
-      expect(ErrorMessage.render).toHaveBeenCalledWith("error")
+      it("does not call WatsonHeader.render", function() {
+        spyOn(WatsonHeader, "render")
+        resultsView.render(userText, watsonRes, holmesRes)
+        expect(WatsonHeader.render).not.toHaveBeenCalledWith()
+      })
+
+      it("does not call WatsonResults.render", function() {
+        spyOn(WatsonResults, "render")
+        resultsView.render(userText, watsonRes, holmesRes)
+        expect(WatsonResults.render).not.toHaveBeenCalledWith(watsonRes)
+      })
+
+      it("calls ErrorMessage.render", function() {
+        spyOn(ErrorMessage, "render")
+        resultsView.render(userText, watsonRes, holmesRes)
+        expect(ErrorMessage.render).toHaveBeenCalledWith(userText)
+      })
     })
   })
 
--- tweetmood/static/js/src/resultsView.js
+++ tweetmood/static/js/src/resultsView.js
@@ -7,35 +7,15 @@
     render: function(user_text, watson_res, holmes_res) {
       if (holmes_res.feeling) {
         return [
-          this._renderHolmesHeader(user_text, holmes_res),
-          this._renderHolmesResult(holmes_res),
-          this._renderWatsonHeader(),
-          this._renderWatsonResult(watson_res)
+          HolmesHeader.render(user_text, holmes_res),
+          HolmesResults.render(holmes_res),
+          WatsonHeader.render(),
+          WatsonResults.render(watson_res)
         ].join("")
       }
       else {
-        return this._renderErrorMessage(user_text)
+        return ErrorMessage.render(user_text)
       }
-    },
-
-    _renderHolmesHeader: function(user_text, holmes_res) {
-      HolmesHeader.render(user_text, holmes_res)
-    },
-
-    _renderHolmesResult: function(holmes_res) {
-      HolmesResults.render(holmes_res)
-    },
-
-    _renderWatsonHeader: function(data) {
-      WatsonHeader.render(data)
-    },
-
-    _renderWatsonResult: function(watson_res) {
-      WatsonResults.render(watson_res)
-    },
-    // done
-    _renderErrorMessage: function(user_text) {
-      ErrorMessage.render(user_text)
     }
 
   }
