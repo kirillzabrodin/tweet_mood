14e5063f8ebff91f71ff4f13a1b79bb8d49b0108
Chris Gilbert <chrisgilbert4@icloud.com>
2019-01-31 22:00:10 +0000

    updated jasmine tests to relfect css tags in html being renders

--- tweetmood/static/js/spec/components/formSpec.js
+++ tweetmood/static/js/spec/components/formSpec.js
@@ -3,7 +3,7 @@ describe("Form", function() {
   describe("render", function() {
     it("returns renders the forms's html", function() {
       spyOn(Button, "render").and.returnValue("<button></button>")
-      var expectResult = '<div class="row col-md-6 offset-md-3 text-center"><form id="london-form" class="form-inline" action="/analysis" method="post"><h1>How does London feel about <input class="form-control" id="city-form-input" type="text" name="text" value=""> right now?</h1><button></button></form></div>'
+      var expectResult = '<div id="input-form" class="row col-md-6 offset-md-3 text-center"><form id="london-form" class="form-inline" action="/analysis" method="post"><h1>How does London feel about <input class="form-control" id="city-form-input" type="text" name="text" value=""> right now?</h1><button></button></form></div>'
       expect(Form.render()).toEqual(expectResult)
     })
 
--- tweetmood/static/js/src/components/form.js
+++ tweetmood/static/js/src/components/form.js
@@ -1,7 +1,7 @@
 var Form = (function() {
 
   function render() {
-    return '<div class="row col-md-6 offset-md-3 text-center"><form id="london-form" class="form-inline" action="/analysis" method="post"><h1>How does London feel about <input class="form-control" id="city-form-input" type="text" name="text" value=""> right now?</h1>' + Button.render() + '</form></div>'
+    return '<div id="input-form" class="row col-md-6 offset-md-3 text-center"><form id="london-form" class="form-inline" action="/analysis" method="post"><h1>How does London feel about <input class="form-control" id="city-form-input" type="text" name="text" value=""> right now?</h1>' + Button.render() + '</form></div>'
   }
 
   return {
@@ -9,18 +9,3 @@ var Form = (function() {
   }
 
 })()
-
-// (function(exports) {
-//
-//   function Form(button = new Button) {
-//     this.button = button
-//   }
-//
-//   Form.prototype = {
-//     render: function() {
-//       return '<div class="row col-md-6 offset-md-3 text-center"><form id="london-form" class="form-inline" action="/analysis" method="post"><h1>How does London feel about <input class="form-control" id="city-form-input" type="text" name="text" value=""> right now?</h1>' + this.button.render() + '</form></div>'
-//     }
-//   }
-//
-//   exports.Form = Form
-// })(this)
